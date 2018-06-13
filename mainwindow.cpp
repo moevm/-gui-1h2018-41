852ce1d31d55099431e7217de3eab9b79b1f8248
Otokomimi <something@example.com>
2018-06-14 00:06:47 +0300

    add mdi documents

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,35 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initWidgets();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initWidgets()
+{
+    QMdiSubWindow* w1 = new QMdiSubWindow(ui->mdiArea);
+    QListWidget* testList1 = new QListWidget(w1);
+    testList1->addItem("123");
+    testList1->addItem("test");
+    testList1->addItem("smth");
+    w1->setWidget(testList1);
+
+    QMdiSubWindow* w2 = new QMdiSubWindow(ui->mdiArea);
+    QListWidget* testList2 = new QListWidget(ui->mdiArea);
+    testList2->addItem("123");
+    testList2->addItem("test");
+    testList2->addItem("smth");
+    w2->setWidget(testList2);
+
+    QMdiSubWindow* w3 = new QMdiSubWindow(ui->mdiArea);
+    QListWidget* testList3 = new QListWidget(ui->mdiArea);
+    testList3->addItem("123");
+    testList3->addItem("test");
+    testList3->addItem("smth");
+    w3->setWidget(testList3);
+
 }
